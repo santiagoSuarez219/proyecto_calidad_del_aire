7416772c47873498ec029c79f854c5f84c371ce5
valentina272393 <valentina272393@correo.itm.edu.co>
2023-06-14 12:01:32 -0500

    VAH main.py

---
+++ entorno_de_desarrollo_python/main.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+
+
+# Crear datos de ejemplo
+
+x = np.linspace(0, 10, 100)  # Array de valores de x de 0 a 10
+
+y = np.sin(x)  # Array de valores de y correspondientes a la función seno
+
+
+
+
+# Crear el gráfico
+
+plt.plot(x, y)
+
+
+
+
+# Personalizar el gráfico
+
+plt.title('Gráfico de la función seno')
+
+plt.xlabel('x')
+
+plt.ylabel('y')
+
+plt.grid(True)
+
+
+
+
+# Mostrar el gráfico
+
+plt.show()
